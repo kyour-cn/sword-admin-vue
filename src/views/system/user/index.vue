424fffb3d5298e2e2e44c4400e5285b273c318ae
Kyour.cn <kyour@vip.qq.com>
2022-12-01 18:04:20 +0800

    [dev] 调整用户管理

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -1,59 +1,50 @@
 <template>
 	<el-container>
-		<el-aside width="200px" v-loading="showGrouploading">
-			<el-container>
-				<el-header>
-					<el-input placeholder="输入关键字进行过滤" v-model="groupFilterText" clearable></el-input>
-				</el-header>
-				<el-main class="nopadding">
-					<el-tree ref="group" class="menu" node-key="id" :data="group" :current-node-key="''" :highlight-current="true" :expand-on-click-node="false" :filter-node-method="groupFilterNode" @node-click="groupClick"></el-tree>
-				</el-main>
-			</el-container>
-		</el-aside>
 		<el-container>
-				<el-header>
-					<div class="left-panel">
-						<el-button type="primary" icon="el-icon-plus" @click="add"></el-button>
-						<el-button type="danger" plain icon="el-icon-delete" :disabled="selection.length==0" @click="batch_del"></el-button>
-						<el-button type="primary" plain :disabled="selection.length==0">分配角色11</el-button>
-						<el-button type="primary" plain :disabled="selection.length==0">密码重置</el-button>
+			<el-header>
+				<div class="left-panel">
+					<el-button type="primary" icon="el-icon-plus" @click="add"></el-button>
+					<el-button type="danger" plain icon="el-icon-delete" :disabled="selection.length==0" @click="batch_del"></el-button>
+					<el-button type="primary" plain :disabled="selection.length==0">分配角色</el-button>
+					<el-button type="primary" plain :disabled="selection.length==0">密码重置</el-button>
+				</div>
+				<div class="right-panel">
+					<div class="right-panel-search">
+						<el-input v-model="search.name" placeholder="登录账号 / 姓名" clearable></el-input>
+						<el-button type="primary" icon="el-icon-search" @click="upsearch"></el-button>
 					</div>
-					<div class="right-panel">
-						<div class="right-panel-search">
-							<el-input v-model="search.name" placeholder="登录账号 / 姓名" clearable></el-input>
-							<el-button type="primary" icon="el-icon-search" @click="upsearch"></el-button>
-						</div>
-					</div>
-				</el-header>
-				<el-main class="nopadding">
-					<scTable ref="table" :apiObj="apiObj" @selection-change="selectionChange" stripe remoteSort remoteFilter>
-						<el-table-column type="selection" width="50"></el-table-column>
-						<el-table-column label="ID" prop="id" width="80" sortable='custom'></el-table-column>
-						<el-table-column label="头像" width="80" column-key="filterAvatar" :filters="[{text: '已上传', value: '1'}, {text: '未上传', value: '0'}]">
-							<template #default="scope">
-								<el-avatar :src="scope.row.avatar" size="small"></el-avatar>
-							</template>
-						</el-table-column>
-						<el-table-column label="登录账号" prop="userName" width="150" sortable='custom' column-key="filterUserName" :filters="[{text: '系统账号', value: '1'}, {text: '普通账号', value: '0'}]"></el-table-column>
-						<el-table-column label="姓名" prop="name" width="150" sortable='custom'></el-table-column>
-						<el-table-column label="所属角色" prop="groupName" width="200" sortable='custom'></el-table-column>
-						<el-table-column label="加入时间" prop="date" width="170" sortable='custom'></el-table-column>
-						<el-table-column label="操作" fixed="right" align="right" width="160">
-							<template #default="scope">
-								<el-button-group>
-									<el-button text type="primary" size="small" @click="table_show(scope.row, scope.$index)">查看</el-button>
-									<el-button text type="primary" size="small" @click="table_edit(scope.row, scope.$index)">编辑</el-button>
-									<el-popconfirm title="确定删除吗？" @confirm="table_del(scope.row, scope.$index)">
-										<template #reference>
-											<el-button text type="primary" size="small">删除</el-button>
-										</template>
-									</el-popconfirm>
-								</el-button-group>
-							</template>
-						</el-table-column>
+				</div>
+			</el-header>
+			<el-main class="nopadding">
+				<scTable ref="table" :apiObj="apiObj" @selection-change="selectionChange" stripe remoteSort remoteFilter>
+					<el-table-column type="selection" width="50"></el-table-column>
+					<el-table-column label="ID" prop="id" width="80" sortable='custom'></el-table-column>
+					<el-table-column label="头像" width="80" column-key="filterAvatar" :filters="[{text: '已上传', value: '1'}, {text: '未上传', value: '0'}]">
+						<template #default="scope">
+							<el-avatar :src="scope.row.avatar" size="small"></el-avatar>
+						</template>
+					</el-table-column>
+					<el-table-column label="登录账号" prop="username" width="150" sortable='custom' column-key="filterUserName" :filters="[{text: '系统账号', value: '1'}, {text: '普通账号', value: '0'}]"></el-table-column>
+					<el-table-column label="姓名" prop="realname" width="150" sortable='custom'></el-table-column>
+					<el-table-column label="手机号" prop="mobile" width="150" sortable='custom'></el-table-column>
+					<el-table-column label="所属角色" prop="groupName" width="200" sortable='custom'></el-table-column>
+					<el-table-column label="加入时间" prop="date" width="170" sortable='custom'></el-table-column>
+					<el-table-column label="操作" fixed="right" align="right" width="160">
+						<template #default="scope">
+							<el-button-group>
+								<el-button text type="primary" size="small" @click="table_show(scope.row, scope.$index)">查看</el-button>
+								<el-button text type="primary" size="small" @click="table_edit(scope.row, scope.$index)">编辑</el-button>
+								<el-popconfirm title="确定删除吗？" @confirm="table_del(scope.row, scope.$index)">
+									<template #reference>
+										<el-button text type="primary" size="small">删除</el-button>
+									</template>
+								</el-popconfirm>
+							</el-button-group>
+						</template>
+					</el-table-column>
 
-					</scTable>
-				</el-main>
+				</scTable>
+			</el-main>
 		</el-container>
 	</el-container>
 
