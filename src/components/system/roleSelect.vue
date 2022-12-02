183a5211c41bc91e6ce91a27bfd53f9837a0c542
Kyour.cn <kyour@vip.qq.com>
2022-12-02 14:46:33 +0800

    [dev] 角色选择组件

---
+++ src/components/system/roleSelect.vue
@@ -0,0 +1,74 @@
+<template>
+	<sc-table-select
+		v-model="state.value"
+		:apiObj="state.apiObj"
+		:table-width="state.tableWidth"
+		clearable
+		collapse-tags
+		collapse-tags-tooltip
+		:props="state.props"
+		@change="onChange"
+	>
+		<template #header="{form, submit}">
+			<el-form :inline="true" :model="form">
+				<el-form-item>
+					<el-select v-model="form.appid" placeholder="选择应用" clearable :teleported="false">
+						<el-option v-for="item in state.appList" :label="item.name" :value="item.id"></el-option>
+					</el-select>
+				</el-form-item>
+				<el-form-item>
+					<el-input v-model="form.name" placeholder="角色名称"></el-input>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="submit">查询</el-button>
+				</el-form-item>
+			</el-form>
+		</template>
+		<el-table-column prop="id" label="ID" width="180"></el-table-column>
+		<el-table-column prop="name" label="名称"></el-table-column>
+	</sc-table-select>
+</template>
+
+<script>
+import {reactive} from "vue";
+import api from '@/api'
+
+export default {
+	name: "roleSelect",
+	props: {
+		tableWidth: Number,
+	},
+	setup(props, {emit}) {
+		const state = reactive({
+			apiObj: api.system.role.list,
+			tableWidth: props.tableWidth? props.tableWidth : 600,
+			props: {
+				label: 'name',
+				value: 'id',
+			},
+			value: [],
+			appList: [],
+			selectedApp: 0
+		})
+
+		const getApp =  async () => {
+			const res = await api.system.app.list.get({
+				pageSize: 50
+			});
+			state.appList = res.data.rows;
+			state.selectedApp = res.data.rows[0].id;
+		}
+		getApp();
+
+		const onChange = (data) => {
+			emit('onChange', data)
+		}
+
+		return { state, onChange}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/template/test.vue
@@ -0,0 +1,36 @@
+
+<template>
+	<el-main>
+
+		<el-card shadow="never" header="角色选择">
+
+			<roleSelect @onChange="change" />
+
+		</el-card>
+	</el-main>
+</template>
+
+<script>
+import roleSelect from "@/components/system/roleSelect";
+
+export default {
+	name: 'tableselect',
+	components: {
+		roleSelect
+	},
+	data() {
+		return {
+		}
+	},
+	methods: {
+		//值变化
+		change(val){
+			this.$message('change事件，返回详情查看控制台')
+			console.log(val)
+		}
+	}
+}
+</script>
+
+<style scoped>
+</style>
